4c40dddad5c4b84886f6916e343e182d3a22aabf
Archiroo <tailieuit.nb@gmail.com>
2022-09-20 23:22:01 +0700

    tbl_administrative finish

--- Trunk/Sever/nda/src/main/java/com/archiiro/nda/RestController/RestAdministrativeUnitController.java
+++ Trunk/Sever/nda/src/main/java/com/archiiro/nda/RestController/RestAdministrativeUnitController.java
@@ -1,9 +1,54 @@
 package com.archiiro.nda.RestController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.archiiro.nda.Dto.AdministrativeUnitDto;
+import com.archiiro.nda.Dto.FunctionDto.ResponseObject;
+import com.archiiro.nda.Dto.FunctionDto.SearchDto;
+import com.archiiro.nda.Service.AdministrativeUnitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/administrative_unit")
 public class RestAdministrativeUnitController {
+    @Autowired
+    private AdministrativeUnitService administrativeUnitService;
+
+    @RequestMapping(value = "/getAllDto", method = RequestMethod.GET)
+    public List<AdministrativeUnitDto> getAllDto() {
+        return this.administrativeUnitService.getAllDto();
+    }
+
+    @RequestMapping(value = "/getById/{id}", method = RequestMethod.GET)
+    public ResponseObject getById(@PathVariable Long id) {
+        return this.administrativeUnitService.findEntityById(id);
+    }
+
+    @RequestMapping(value = "/createDto", method = RequestMethod.POST)
+    public ResponseObject createDto(@RequestBody AdministrativeUnitDto dto) {
+        return this.administrativeUnitService.createDto(null, dto);
+    }
+
+    @RequestMapping(value = "/updateDto/{id}", method = RequestMethod.PUT)
+    public ResponseObject updateDto(@PathVariable Long id, @RequestBody AdministrativeUnitDto dto) {
+        return this.administrativeUnitService.updateDto(id, dto);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseObject deleteDto(@PathVariable Long id) {
+        return this.administrativeUnitService.deleteDto(id);
+    }
+
+    @RequestMapping(value = "/page/{pageIndex}/{pageSize}", method = RequestMethod.GET)
+    public Page<AdministrativeUnitDto> getPageDto(@PathVariable Integer pageIndex, @PathVariable Integer pageSize) {
+        return this.administrativeUnitService.getPageDto(pageIndex, pageSize);
+    }
+
+    @RequestMapping(value = "/searchByPage")
+    public Page<AdministrativeUnitDto> searchByPage(@RequestBody SearchDto searchDto) {
+        return this.administrativeUnitService.searchByPage(searchDto);
+    }
 }
--- Trunk/Sever/nda/src/main/java/com/archiiro/nda/Service/ServiceImpl/AdministrativeUnitServiceImpl.java
+++ Trunk/Sever/nda/src/main/java/com/archiiro/nda/Service/ServiceImpl/AdministrativeUnitServiceImpl.java
@@ -1,7 +1,157 @@
 package com.archiiro.nda.Service.ServiceImpl;
 
+import com.archiiro.nda.Domain.AdministrativeUnit;
+import com.archiiro.nda.Dto.AdministrativeUnitDto;
+import com.archiiro.nda.Dto.FunctionDto.ResponseObject;
+import com.archiiro.nda.Dto.FunctionDto.SearchDto;
+import com.archiiro.nda.Reponsitory.AdministrativeUnitRepository;
+import com.archiiro.nda.Service.AdministrativeUnitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class AdministrativeUnitServiceImpl {
+public class AdministrativeUnitServiceImpl implements AdministrativeUnitService {
+
+    @Autowired
+    private  AdministrativeUnitRepository administrativeUnitRepos;
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<AdministrativeUnitDto> getAllDto() {
+        return this.administrativeUnitRepos.getAllDto();
+    }
+
+    @Override
+    public ResponseObject findEntityById(Long id) {
+        AdministrativeUnit entity = null;
+        if(id != null) {
+            Optional<AdministrativeUnit> administrativeUnitOptional = this.administrativeUnitRepos.findById(id);
+            if(administrativeUnitOptional.isPresent()) {
+                entity = administrativeUnitOptional.get();
+                return new ResponseObject("200", "Success", entity);
+            }
+            return new ResponseObject("404", "Can't not find Adminstrative witd ID: " + id, null);
+        }
+        return new ResponseObject("500", "Error" , null);
+    }
+
+    @Override
+    public ResponseObject createDto(Long id, AdministrativeUnitDto dto) {
+        return this.updateDto(null, dto);
+    }
+
+    @Override
+    public ResponseObject updateDto(Long id, AdministrativeUnitDto dto) {
+        AdministrativeUnit entity = null;
+        LocalDateTime currentDate = LocalDateTime.now();
+        if(id != null) {
+            Optional<AdministrativeUnit> administrativeUnitOptional = this.administrativeUnitRepos.findById(id);
+            if (administrativeUnitOptional.isPresent()) {
+                entity = administrativeUnitOptional.get();
+            }
+        } else if(dto != null && dto.getId() != null) {
+            Optional<AdministrativeUnit> administrativeUnitOptional = this.administrativeUnitRepos.findById(id);
+            if (administrativeUnitOptional.isPresent()) {
+                entity = administrativeUnitOptional.get();
+            }
+        }
+        if(entity == null) {
+            entity = new AdministrativeUnit();
+            entity.setDateCreate(currentDate);
+        }
+        if(dto.getCode() != null) {
+            entity.setCode(dto.getCode());
+        }
+        if(dto.getName() != null) {
+            entity.setName(dto.getName());
+        }
+        // Parent
+        if(dto.getParent() != null) {
+            AdministrativeUnit parentEntity = null;
+            if(dto.getParent().getId() != null) {
+                Optional<AdministrativeUnit> administrativeUnitParentOptional = this.administrativeUnitRepos.findById(dto.getParent().getId());
+                if(administrativeUnitParentOptional != null) {
+                    parentEntity = administrativeUnitParentOptional.get();
+                    entity.setParent(parentEntity);
+                    if(parentEntity.getLevel() != null && parentEntity.getLevel() > 0) {
+                        entity.setLevel(parentEntity.getLevel() + 1);
+                    }
+                }
+            }
+        } else {
+            entity.setLevel(1); // Cấp tỉnh
+            entity.setParent(null);
+        }
+        entity = this.administrativeUnitRepos.save(entity);
+        if(entity != null) {
+            return new ResponseObject("200", "Success", entity);
+        }
+        return new ResponseObject("500", "Error", null);
+    }
+
+    @Override
+    public ResponseObject deleteDto(Long id) {
+        AdministrativeUnit entity = null;
+        if(id != null) {
+            Optional<AdministrativeUnit> administrativeUnitOptional = this.administrativeUnitRepos.findById(id);
+            if(administrativeUnitOptional.isPresent()) {
+                entity = administrativeUnitOptional.get();
+                this.administrativeUnitRepos.delete(entity); // Trường hợp xóa tỉnh
+                return new ResponseObject("200", "Success", entity);
+            }
+            return new ResponseObject("404", "Can't not find Adminstrative witd ID: " + id, null);
+        }
+        return new ResponseObject("500", "Error" , null);
+    }
+
+    @Override
+    public Page<AdministrativeUnitDto> getPageDto(int pageIndex, int pageSize) {
+        Pageable pageable = PageRequest.of(pageIndex, pageSize);
+        return this.administrativeUnitRepos.getPageDto(pageable);
+    }
+
+    @Override
+    public Page<AdministrativeUnitDto> searchByPage(SearchDto searchDto) {
+        if(searchDto != null && searchDto.getPageIndex() != null && searchDto.getPageSize() != null) {
+            int pageIndex = searchDto.getPageIndex();
+            int pageSize = searchDto.getPageSize();
+            if(pageIndex > 0) {
+                pageIndex = pageIndex - 1;
+            } else {
+                pageIndex = 0;
+            }
+            Pageable pageable = PageRequest.of(pageIndex, pageSize);
+            String sql = "Select new com.archiiro.nda.Dto.AdministrativeUnitDto(entity, true) From AdministrativeUnit entity where (1=1)";
+            String sqlCount = "Select count(entity.id) From AdministrativeUnit entity Where (1=1) ";
+            String whereClause = "";
+            String orderBy = " Order by entity.dateCreate DESC";
+            if(searchDto.getNameOrCode() != null) {
+                whereClause += " And entity.code Like :nameOrCode Or entity.name Like :nameOrCode ";
+            }
+            sql += whereClause + orderBy;
+            sqlCount = whereClause;
+            Query q = manager.createQuery(sql, AdministrativeUnitDto.class);
+            Query qCount = manager.createQuery(sqlCount);
+            if(searchDto.getNameOrCode() != null) {
+                q.setParameter("nameOrCode", "%" + searchDto.getNameOrCode() + "%");
+                qCount.setParameter("nameOrCode", "%" + searchDto.getNameOrCode() + "%");
+            }
+            q.setFirstResult(pageIndex * pageSize);
+            q.setMaxResults(pageSize);
+            Long numberResult = (Long) qCount.getSingleResult();
+            Page<AdministrativeUnitDto> page = new PageImpl<>(q.getResultList(), pageable, pageSize);
+            return page;
+        }
+        return null;
+    }
 }
